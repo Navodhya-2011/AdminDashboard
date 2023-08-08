9af8d544552478adf47f67c96b8e207a1c69dcea
Navodhya Yasisuru <navodhya2011@gmail.com>
2023-08-08 18:55:37 +0530

    Initial Commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>navoadmin</span>
+      <span>© Navod Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
